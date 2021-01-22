08c2451b3e678d50e73fb7c9cf8327dc2dac946d
Огурчик <sergeyazarov1006@gmail.com>
2021-01-22 21:42:31 +0200

    final

--- Lab 5.1.cpp
+++ Lab 5.1.cpp
@@ -8,11 +8,11 @@ LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 LRESULT CALLBACK newWndProc(HWND, UINT, WPARAM, LPARAM);
 
 
-void DrawBitmap(HDC hDC, int x, int y, HBITMAP hBitmap);
+void DrawBitmap(HWND, HDC hDC, int x, int y, HBITMAP hBitmap);
 HWND Button;
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
 {
-	TCHAR szClassName[] = L"Мой класс";
+	TCHAR szClassName[] = TEXT("Мой класс");
 	MSG msg;
 	WNDCLASSEX wc;
 	wc.cbSize = sizeof(wc);
@@ -25,31 +25,31 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInstance, PSTR szCmdLine, int
 	wc.hIcon = LoadIcon(NULL, IDI_WINLOGO);
 	wc.hIconSm = LoadIcon(NULL, IDI_WINLOGO);
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wc.hbrBackground = static_cast<HBRUSH>(GetStockObject(WHITE_BRUSH));
 	wc.hInstance = hInst;
 	if (!RegisterClassEx(&wc)) {
 
-		MessageBox(NULL, L"Не получилось зарегистрировать класс!", L"Ошибка", MB_OK);
+		MessageBox(NULL, TEXT("Не получилось зарегистрировать класс!"), TEXT("Ошибка"), MB_OK);
 		return NULL;
 	}
-	HWND hWnd = CreateWindowEx(0,
+	HWND hWnd = CreateWindow(
 		szClassName,
-		_T("Лабораторная работа №5"),
-		WS_OVERLAPPED | WS_SYSMENU | WS_CAPTION | WS_MINIMIZEBOX,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
+		TEXT("Лабораторная работа №5"),
+		WS_OVERLAPPED | WS_SYSMENU | WS_VSCROLL,
 		CW_USEDEFAULT,
 		NULL,
+		CW_USEDEFAULT,
 		NULL,
+		static_cast<HWND>(NULL),
 		NULL,
-		hInst,
+		static_cast<HINSTANCE>(hInst),
 		NULL);
 	if (!hWnd)
 	{
 		MessageBox(NULL, TEXT("Не получилось создать окно!"), TEXT("Ошибка"), MB_OK);
 		return NULL;
 	}
-	Button = CreateWindow(TEXT("Button"), TEXT("Press me"), WS_CHILD | WS_VISIBLE, 500, 300, 130, 130, hWnd, reinterpret_cast<HMENU>(1), hInst, NULL);
+	Button = CreateWindow(TEXT("Button"), TEXT("Press me"), WS_CHILD | WS_VISIBLE | CS_DBLCLKS | WS_SYSMENU, 500, 300, 130, 130, hWnd, reinterpret_cast<HMENU>(1), hInst, NULL);
 	ShowWindow(hWnd, iCmdShow);
 	UpdateWindow(hWnd);
 
@@ -59,6 +59,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInstance, PSTR szCmdLine, int
 		DispatchMessage(&msg);
 	}
 	return msg.wParam;
+	DestroyWindow(Button);
 }
 
 
@@ -70,13 +71,14 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	HDC hDC;
 	PAINTSTRUCT ps;
 	RECT rect;
+
 	HBRUSH hBrush;
 	switch (uMsg)
 	{
 	case  WM_CREATE:
 		InvalidateRect(hWnd, NULL, TRUE);
-		break;
 
+		break;
 	case WM_PAINT:
 		RedrawWindow(hWnd, NULL, NULL, RDW_ERASENOW);
 		hDC = GetDC(hWnd);
@@ -91,43 +93,56 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		PostQuitMessage(0);
 		break;
 	case WM_COMMAND:
-
+	{
+	case WM_LBUTTONDBLCLK:
 		if (lParam == reinterpret_cast<LPARAM>(Button))
 		{
 			InvalidateRect(hWnd, NULL, TRUE);
 			SetWindowLongPtr(hWnd, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(&newWndProc));
 		}
 		break;
-	}
 
+	}
+	}
 	return DefWindowProc(hWnd, uMsg, wParam, lParam);
 }
 
 
 LRESULT CALLBACK newWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+
 	HDC hDC;
 	PAINTSTRUCT ps;
 	RECT rect;
+	GetClientRect(hWnd, &rect);
+	RECT rect_old;
 	HBITMAP hBit = static_cast<HBITMAP>(LoadImage(NULL, TEXT("hBitMap.bmp"), NULL, 500, 500, LR_LOADFROMFILE));
 
 	switch (uMsg)
 	{
-	case WM_CREATE:
-		break;
+	case WM_SIZING:
+		hDC = GetDC(hWnd);
+		GetClientRect(hWnd, &rect_old);
+		StretchBlt(hDC, rect_old.left, rect_old.top, rect_old.right, rect_old.bottom, hDC, rect.left, rect.top, rect.right, rect.bottom, DSTINVERT);
+		ReleaseDC(hWnd, hDC);
 	case WM_PAINT:
 		RedrawWindow(hWnd, NULL, NULL, RDW_ERASENOW);
 		hDC = BeginPaint(hWnd, &ps);
-		DrawBitmap(hDC, 300, 300, hBit);
+		DrawBitmap(hWnd, hDC, 300, 150, hBit);
 		EndPaint(hWnd, &ps);
 		break;
 	case WM_COMMAND:
+	{
+	case WM_LBUTTONDBLCLK:
+	{
 		if (lParam == reinterpret_cast<LPARAM>(Button))
 		{
 			InvalidateRect(hWnd, NULL, TRUE);
 			SetWindowLongPtr(hWnd, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(&WndProc));
-			break;
 		}
+	}
+	break;
+	}
 	case WM_DESTROY:
 
 		PostQuitMessage(0);
@@ -137,14 +152,15 @@ LRESULT CALLBACK newWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 }
 
 
-void DrawBitmap(HDC hDC, int x, int y, HBITMAP hBitmap)
+void DrawBitmap(HWND hWnd, HDC hDC, int x, int y, HBITMAP hBitmap)
 {
+	RECT rect;
 	HBITMAP hbm, hOldbm;
 	HDC hMemDC;
 	BITMAP bm;
 	POINT  ptSize, ptOrg;
 	hMemDC = CreateCompatibleDC(hDC);
-
+	GetClientRect(hWnd, &rect);
 	hOldbm = (HBITMAP)SelectObject(hMemDC, hBitmap);
 
 	if (hOldbm)
@@ -168,5 +184,6 @@ void DrawBitmap(HDC hDC, int x, int y, HBITMAP hBitmap)
 
 		SelectObject(hMemDC, hOldbm);
 	}
+
 	DeleteDC(hMemDC);
 }
