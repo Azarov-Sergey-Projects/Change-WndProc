28a5e0f8ddebdcf5cf8daebbac265d6d22b47c9d
Огурчик <sergeyazarov1006@gmail.com>
2021-01-22 11:23:21 +0200

    Добавьте файлы проекта.

---
+++ Lab 5.1.cpp
@@ -0,0 +1,10 @@
+﻿// Lab 5.1.cpp : Определяет точку входа для приложения.
+//
+
+#include <Windows.h>
+
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
+{
+
+}
